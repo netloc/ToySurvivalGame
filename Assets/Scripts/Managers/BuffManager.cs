d460d14cf10ea9010bffc2fbbbefa5463b1c7362
netloc <david.a.davis@live.com>
2015-05-18 15:57:36 -0500

    Working on Buff Spawns.
    Added the Buff Manager and am working on getting the buffs to randomly
    generate.

--- Assets/Scripts/Managers/BuffManager.cs
+++ Assets/Scripts/Managers/BuffManager.cs
@@ -1,46 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum BuffNames
-{
-	Shield,
-	Speed
-}
+public class BuffManager : MonoBehaviour {
 
-public class BuffManager : MonoBehaviour 
-{
-	GameObject player;
-	PlayerHealth playerHealth;
-	PlayerBuffs playerBuffs;
-	public BuffNames Buff;
+	public GameObject Buff;
 
-	void Awake ()
-	{
-		player = GameObject.FindGameObjectWithTag ("Player");
-		playerHealth = player.GetComponent <PlayerHealth> ();
-		playerBuffs = player.GetComponent <PlayerBuffs> ();
+	// Use this for initialization
+	void Start () {
+	
 	}
-		
+	
 	// Update is called once per frame
-	void Update () 
-	{
-
-	}
-
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.gameObject == player) {
-			switch (Buff) {
-			case BuffNames.Shield:
-				playerBuffs.AddShieldBonus ();
-				break;
-
-			default:
-				// Do nothing the buff is not implemented
-				break;
-			}
-
-			Destroy (this.gameObject);
-		}
+	void Update () {
+	
 	}
 }
