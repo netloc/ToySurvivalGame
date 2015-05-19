3ad687184333eabdd47d04f9233c10aaa3077c75
netloc <david.a.davis@live.com>
2015-05-19 10:29:34 -0500

    test

---
+++ Assets/Scripts/Buffs/BuffSpawnL1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuffSpawnL1 : MonoBehaviour {
+
+	public int[][] Coords;
+
+	void Awake() {
+		Coords = new int[1][] { new int[3] { 1, 1, 1 } };
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
