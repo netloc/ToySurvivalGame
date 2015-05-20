c3e1ce3f9ab96a1bdb15e5027b274c1174714500
netloc <david.a.davis@live.com>
2015-05-20 09:07:26 -0500

    Fixed Merge Issues

--- Assets/Scripts/Buffs/BuffRotation.cs
+++ Assets/Scripts/Buffs/BuffRotation.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class BuffRotation : MonoBehaviour {
 
-	Transform transform;
+	Transform itemTransform;
 
 	void Awake () {
-		transform = GetComponent <Transform> ();
+		itemTransform = GetComponent <Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		//transform.rotation.Sdwet (transform.rotation.x, rotationValue, transform.rotation.z, transform.rotation.w);
-		transform.Rotate (9f, 0f, 9f);
+		itemTransform.Rotate (9f, 0f, 9f);
 	}
 }
