68f443e8d87c18f1c25d535e163afca42bc853cb
necromonopoly <lampe.keith@gmail.com>
2015-05-26 08:40:48 -0500

    Level 02 Goodness - take 2
    new assets for Level 02:
    structures - house 01, house 02, well, windmill, chapel, fencepost,
    fencewall, barrel, wood box, chest (WIP)
    characters - demon (Animated (new BossAC), collision detection from
    shooting not working)

---
+++ Assets/Scripts/Buffs/ChestHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestHandler : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		anim = GetComponent <Animator> ();
+		anim.applyRootMotion = true;
+	}
+
+	void Update()
+	{
+
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if ("Player" == other.gameObject.tag)
+		{
+			anim.SetTrigger("IsTouched");
+		}
+		else
+		{
+			anim.ResetTrigger("IsTouched");
+		}
+	}
+
+
+}
